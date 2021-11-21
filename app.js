132e8a8ca4e029648a4caf59315a41b9d358981e
elagutie <elagutie@bancolombia.com.co>
2021-11-21 09:53:15 -0500

    202111210952 -Se crea gitignore y archivos de configuracion común

---
+++ app.js
@@ -0,0 +1,17 @@
+require('colors');
+
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
+console.clear();
+
+
+
+const main = async() =>{
+    console.log('Hola mundo');
+
+    mostrarMenu();
+    // pausa();
+
+}
+
+main();
