ef9e2b0201944bc8e26930d6cc2658b6fcd8440d
elagutie <elagutie@bancolombia.com.co>
2021-11-21 10:11:49 -0500

    47 - Repetir el menu de forma infinita

--- app.js
+++ app.js
@@ -9,8 +9,13 @@ console.clear();
 const main = async() =>{
     console.log('Hola mundo');
 
-    mostrarMenu();
-    // pausa();
+    let opt = '';
+
+    do {
+        opt = await mostrarMenu();
+        console.log({opt});
+        if ( opt !== '0') await pausa();
+    } while ( opt !== '0');
 
 }
 
