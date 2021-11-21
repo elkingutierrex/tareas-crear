a042af983e2965756f070c82c933fb73e046f7fa
elagutie <elagutie@bancolombia.com.co>
2021-11-21 10:31:50 -0500

    49. Construir el menú interactivo - Inquirer

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
+const { inquirerMenu } = require('./helpers/inquirer');
+
 require('colors');
 
-const { mostrarMenu, pausa } = require('./helpers/mensajes');
-
 console.clear();
-
-
 
 const main = async() =>{
     console.log('Hola mundo');
@@ -12,9 +10,8 @@ const main = async() =>{
     let opt = '';
 
     do {
-        opt = await mostrarMenu();
-        console.log({opt});
-        if ( opt !== '0') await pausa();
+        opt = await inquirerMenu();
+        console.log({opt});        
     } while ( opt !== '0');
 
 }
---
+++ helpers/inquirer.js
@@ -0,0 +1,30 @@
+require('colors');
+const inquirer = require('inquirer');
+
+
+const preguntas = [
+    { 
+        type: 'list',
+        name: 'opcion',
+        message: '¡Qué deseas hacer?',
+        choices: ['opt1','opt2','opt3']
+        
+    }
+]
+
+const inquirerMenu = async() =>{
+
+    // console.clear();
+    console.log('============================='.green);
+    console.log('    Seleccione una opción'.green);
+    console.log('=============================\n'.green);
+
+    const opt = await inquirer.prompt( preguntas );
+    return opt;
+
+}
+
+
+module.exports = {
+    inquirerMenu
+}
